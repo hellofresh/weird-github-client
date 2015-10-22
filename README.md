4b031b6b9ab279f1fd0d26f2b9d344186803bedc
Nuno Simaria <nsimaria@gmail.com>
2015-10-22 18:57:30 +0200

    small code improvements and docs

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
-# hellofresh/duplica-repo
+# hellofresh/duplicate-repo
 
-duplicates a repo on github with a new name and adds users to it
+Main purpose of this repo is to automate the creation of a new private repo for candidates to submit their technical tests.
+
+# usage
+
+Copy the `.env.dist` file to `.env` and add your github api token there.
+
+IMPORTANT: The token must have the `repo` and `admin:org` scopes.
+
+```
+./bin/index.js
+
+```
+
+You will be asked for a repo name. You probably want to use `backend-test`, `frontend-test`, `ios-test`, `android-test`, `data-engineering-test`, `data-science-test`, etc...
+
+The second prompt is for you to add the candidate's github username, which someone from HR must have given to you.
+
+And that's it.
+
+## Requisites
+
+You must have `npm` installed.
+
+
--- bin/index.js
+++ bin/index.js
@@ -4,43 +4,32 @@ require('dotenv').load();
 
 
 module.exports = (function() {
-
-    var output = function (error, stdout, stderr){
-        console.log('error:');
-        console.log(error);
-        console.log('stdout');
-        console.log(stdout);
-        console.log('stderr');
-        console.log(stderr);
-    };
-
-
-
     var inquirer = require('inquirer');
-    var settings = {repo:null, user: null};
+    var repo = null;
+    var user = null;
     var exec = require('child_process').exec;
+    var org = 'hellofresh';
+    var team = 1824421;
 
-    var GitHubApi = require("github");
+    // initalize github client
+    var GitHubApi = require('github');
     
     var github = new GitHubApi({
-        // required
-        version: "3.0.0",
-        // optional
+        version: '3.0.0',
         debug: true,
-        protocol: "https",
-        host: "api.github.com", // should be api.github.com for GitHub
-        pathPrefix: null, // for some GHEs; none for GitHub
+        protocol: 'https',
+        host: 'api.github.com',
+        pathPrefix: null,
         timeout: 5000,
         headers: {
-            "user-agent": "HelloFresh Candidate Repo Script" // GitHub is happy with a unique user agent
+            'user-agent': 'HelloFresh Candidate Repo Script'
         }
     });
 
     github.authenticate({
-        type: "oauth",
+        type: 'oauth',
         token: process.env.GITHUB_API_TOKEN
     });
-
 
     inquirer.prompt([
         {
@@ -56,21 +45,26 @@ module.exports = (function() {
 
     ], function(answers) {
         if (answers.repo) {
-            settings.repo = answers.repo;
+            repo = answers.repo;
         }
 
         if (answers.user) {
-            settings.user = answers.user;
+            user = answers.user;
         }
 
-        if (!settings.repo || !settings.user) {
-            console.log ("bad info!");
+        if (!repo || !user) {
+            console.log ('bad info!');
             return;
         }
 
-        //github.orgs.getTeams ({"org": "hellofresh"}, function (err, res){    console.log(JSON.stringify(res));});
-        github.repos.createFromOrg ({"name":  settings.user + '-' + settings.repo, "private" : true, "org": "hellofresh", "team_id":"406537"}, function (){
-            exec ('git clone git@github.com:hellofresh/' + settings.repo + ' && cd ' + settings.repo + ' && git remote set-url origin git@github.com:hellofresh/' + settings.user + '-' + settings.repo + ' && git push origin master && cd .. && rm -rf ' + settings.repo , function (error, stdout, stderr) {
+        var target = user + '-' + repo;
+
+//        uncomment this if you want to just check the team ids, you might need it if you want to change teams.
+//        github.orgs.getTeams ({'org': 'hellofresh'}, function (err, res){    console.log(JSON.stringify(res));});
+//        return;
+
+        github.repos.createFromOrg ({'name':  target, 'private' : true, 'org': org, 'team_id':'406537'}, function (){
+            exec ('git clone git@github.com:hellofresh/' + repo + ' && cd ' + repo + ' && git remote set-url origin git@github.com:hellofresh/' + target + ' && git push origin master && cd .. && rm -rf ' + repo , function (error, stdout, stderr) {
                 if (!error) {
                     console.log (stdout);
                     console.log ('finished!');
@@ -82,13 +76,9 @@ module.exports = (function() {
                 }
             });
 
-            github.repos.addCollaborator ({"repo":  settings.user + '-' + settings.repo, "user": 'hellofresh', "collabuser" : settings.user}, function (err, res){  if (!err) console.log ("added " + settings.user + ' to ' + settings.user + '-' + settings.repo)});
-
+            github.repos.addCollaborator ({'repo':  target, 'user': org, 'collabuser' : user}, function (err, res){  if (!err) console.log ('added ' + user + ' to ' + target)});
         });
-
     });
-
-
 })();
 
 
