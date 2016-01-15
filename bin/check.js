69fac41f4f0b702e15f8044db43c8ed47209c443
Nuno Simaria <nsimaria@gmail.com>
2016-01-15 13:08:44 +0100

    checking integrity of pr's for audit

---
+++ bin/check.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+require('dotenv').load();
+
+module.exports = (function() {
+
+    var GithubWrapper = require('../lib/GithubWrapper');
+    var Commander = require('../lib/Commander');
+
+    var githubWrapper = new GithubWrapper();
+
+    var commander = new Commander(githubWrapper);
+    commander.check();
+
+}());
