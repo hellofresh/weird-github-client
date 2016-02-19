a6f3ac3471c5da74397d8941e13161866d8c3ad3
Nuno Simaria <nsimaria@gmail.com>
2016-02-19 16:11:20 +0100

    halfway integrating google drive

--- bin/developer-stats.js
+++ bin/developer-stats.js
@@ -5,11 +5,13 @@ require('dotenv').load();
 module.exports = (function() {
 
     var GithubWrapper = require('../lib/GithubWrapper');
+    var GoogleSpreadsheetWrapper = require('../lib/GoogleSpreadsheetWrapper');
     var Commander = require('../lib/DeveloperStatsCommander');
 
     var githubWrapper = new GithubWrapper();
+    var googleSpreadsheetWrapper = new GoogleSpreadsheetWrapper();
 
-    var commander = new Commander(githubWrapper);
+    var commander = new Commander(githubWrapper, googleSpreadsheetWrapper);
     commander.start();
 
 }());
