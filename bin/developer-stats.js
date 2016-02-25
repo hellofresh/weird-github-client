2806c57d2016ee483c2e854d5b8b1acc544b528b
Nuno Simaria <nsimaria@gmail.com>
2016-02-25 01:06:04 +0100

    big restrucutre, process envs on bin file, and others

--- bin/developer-stats.js
+++ bin/developer-stats.js
@@ -6,12 +6,39 @@ module.exports = (function() {
 
     var GithubWrapper = require('../lib/GithubWrapper');
     var GoogleSpreadsheetWrapper = require('../lib/GoogleSpreadsheetWrapper');
+    var SlackWrapper = require('../lib/SlackWrapper');
     var Commander = require('../lib/DeveloperStatsCommander');
 
-    var githubWrapper = new GithubWrapper();
-    var googleSpreadsheetWrapper = new GoogleSpreadsheetWrapper(process.env.DEV_STATS_GOOGLE_DRIVE_SPREADSHEET_ID);
+    var githubWrapper = null;
+    var googleSpreadsheetWrapper = null;
+    var slackWrapper = null;
 
-    var commander = new Commander(githubWrapper, googleSpreadsheetWrapper);
+    githubWrapper = new GithubWrapper({
+        token: process.env.GITHUB_API_TOKEN,
+    });
+
+    if (process.env.DEV_STATS_USE_GOOGLE_SPREADSHEET === 'true') {
+        googleSpreadsheetWrapper = new GoogleSpreadsheetWrapper({
+            spreadsheetId: process.env.DEV_STATS_GOOGLE_SPREADSHEET_ID,
+        });
+    }
+
+    if (process.env.DEV_STATS_USE_SLACK === 'true') {
+        slackWrapper = new SlackWrapper ({
+            username: process.env.DEV_STATS_SLACK_USERNAME,
+            emoji: process.env.DEV_STATS_SLACK_EMOJI,
+            url: process.env.DEV_STATS_SLACK_URL,
+        });
+    }
+
+    var commander = new Commander({
+        authorWorksheetIndex: process.env.DEV_STATS_GOOGLE_AUTHOR_WORKSHEET_INDEX,
+        repoWorksheetIndex: process.env.DEV_STATS_GOOGLE_REPO_WORKSHEET_INDEX,
+        contributionsWorksheetIndex: process.env.DEV_STATS_GOOGLE_CONTRIBUTIONS_WORKSHEET_INDEX,
+        defaultOrganization: process.env.DEFAULT_ORGANIZATION,
+        repos: process.env.DEV_STATS_REPOS.split(','),
+        'nonUsers': process.env.DEV_STATS_NON_USERS.split(','),
+    }, githubWrapper, googleSpreadsheetWrapper, slackWrapper);
+
     commander.start();
-
 }());
