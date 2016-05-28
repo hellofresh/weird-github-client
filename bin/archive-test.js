0eb2bfa24c17a398ffa0e763e6138e65e55c648d
Nuno Simaria <nsimaria@gmail.com>
2016-05-29 00:40:17 +0200

    structuring archive-test

--- bin/archive-test.js
+++ bin/archive-test.js
@@ -4,8 +4,11 @@ require('dotenv').load();
 
 module.exports = (function() {
 
+    var name = 'archive-test';
+    var message = 'Archives a branch of a repo, adding it as a git subtree to another repo. In the end you may choose to delete the archived repo. You will lose your PRs and comments but you will keep your commit history.';
+
     var Commander = require('../lib/Commander');
-    var Program = require('../lib/ArchiveTest');
+    var Program = require('../lib/Programs/ArchiveTest');
 
     var commander = new Commander();
     var program = new Program (commander);
