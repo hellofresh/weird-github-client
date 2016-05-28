b6f92867529c7025758ada4d759a3069b66f0d78
Nuno Simaria <nsimaria@gmail.com>
2016-05-28 21:39:51 +0200

    cleaning up before run

---
+++ bin/normalize-teams.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+require('dotenv').load();
+
+module.exports = (function() {
+
+    var GithubWrapper = require('../lib/GithubWrapper');
+    var Commander = require('../lib/NormalizeTeamsCommander');
+
+    var githubWrapper = new GithubWrapper({
+        token: process.env.GITHUB_API_TOKEN,
+    });
+
+    var commander = new Commander(githubWrapper);
+    commander.start();
+
+}());
