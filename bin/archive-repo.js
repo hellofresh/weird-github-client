9b43c019d87498e954f123386c8d9bd2932c546f
Nuno Simaria <nsimaria@gmail.com>
2016-05-30 11:49:32 +0200

    renaming archive test to archive repo

--- bin/archive-repo.js
+++ bin/archive-repo.js
@@ -4,11 +4,11 @@ require('dotenv').load();
 
 module.exports = (function() {
 
-    var name = 'archive-test';
+    var name = 'archive-repo';
     var message = 'Archives a branch of a repo, adding it as a git subtree to another repo. In the end you may choose to delete the archived repo. You will lose your PRs and comments but you will keep your commit history.';
 
     var Commander = require('../lib/Commander');
-    var Program = require('../lib/Programs/ArchiveTest');
+    var Program = require('../lib/Programs/ArchiveRepo');
 
     var commander = new Commander(name, message);
     var program = new Program (commander);
